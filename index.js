983fa3000636a2be7914e3c6b6e1f1b0ea8b8b85
Zekkie Berkane <zekkieberkane2@gmail.com>
2019-03-20 12:28:40 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function avadaKedavraFeature(feature) {
+ 	const destructedStr = feature.split(".");
+ 	let target;
+ 	
+
+ 	if(destructedStr.length > 1) {
+ 		 target = eval(destructedStr[0]);
+ 		 if (destructedStr.length <= 2) {
+	 	if(target[destructedStr[1]].constructor) {
+	 		delete target[destructedStr[1]];
+	 	}else {
+	 		target[destructedStr[1]] = undefined;
+	 	}
+		 } else if(destructedStr.length === 3) {
+		 	target.prototype[destructedStr[2]] = undefined
+		 } 
+ 	}	
+	else {
+	 	try {
+	 		target = eval(feature)
+	 		if(target.constructor) {
+	 			delete window[feature];
+	 		}else {
+	 			window[feature] = undefined;
+	 		}
+	 	} catch(e) {
+	 		console.log(e)
+	 	}
+	 }
+}
+
+avadaKedavraFeature("localStorage");
+
+
+if(window.localStorage) {
+	console.log("Has storage")
+} else {
+	console.log("no storage")
+}
